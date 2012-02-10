e1bf915161fb1da088f735833486abf2b6999c80
skironnet@gmail.com <skironnet@gmail.com@dd52ff48-e55d-9dd6-d6ed-6137da71bd58>
2012-02-10 23:54:02 +0000

    units tests added for annotated parsing

--- trunk/src/main/java/com/googlecode/jcsv/annotations/processors/BooleanProcessor.java
+++ trunk/src/main/java/com/googlecode/jcsv/annotations/processors/BooleanProcessor.java
@@ -10,12 +10,15 @@ import com.googlecode.jcsv.annotations.ValueProcessor;
  */
 public class BooleanProcessor implements ValueProcessor<Boolean> {
 	/**
-	 * Converts value into a boolean using {@link Boolean#parseBoolean(String)}
+	 * Converts value into a boolean.
+	 * Returns true, if value is "true" or "1", false otherwise.
 	 *
 	 * @return Boolean the result
 	 */
 	@Override
 	public Boolean processValue(String value) {
-		return Boolean.parseBoolean(value);
+		if (value == null) return false;
+		
+		return value.equals("1") || value.toLowerCase().equals("true");		
 	}
 }
