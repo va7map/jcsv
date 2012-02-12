8131cf367bd01ffd620932bc796b0637aca7747a
skironnet@gmail.com <skironnet@gmail.com@dd52ff48-e55d-9dd6-d6ed-6137da71bd58>
2012-02-12 14:38:56 +0000

    [maven-release-plugin]  copy for tag jcsv-1.3.0

---
+++ tags/jcsv-1.3.0/src/test/java/com/googlecode/jcsv/reader/internal/AnnotationEntryParserTest.java
@@ -0,0 +1,65 @@
+package com.googlecode.jcsv.reader.internal;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.googlecode.jcsv.annotations.MapToColumn;
+import com.googlecode.jcsv.reader.CSVEntryParser;
+
+public class AnnotationEntryParserTest {
+	
+	@Test
+	public void testParseEntry() {
+		CSVEntryParser<Person> entryParser = new AnnotationEntryParser<Person>(Person.class);
+		
+		Person expected = new Person("Hans", "im Glück", 18);
+		String[] data = {"Hans", "im Glück", "18"};
+		Person result = entryParser.parseEntry(data);
+		assertEquals(expected, result);
+	}
+
+	private static class Person {
+		@MapToColumn(column=0)
+		private String firstName;
+		
+		@MapToColumn(column=1)
+		private String lastName;
+		
+		@MapToColumn(column=2)
+		private int age;
+		
+		public Person() {
+		}
+		
+		public Person(String firstName, String lastName, int age) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.age = age;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Person other = (Person) obj;
+			if (age != other.age)
+				return false;
+			if (firstName == null) {
+				if (other.firstName != null)
+					return false;
+			} else if (!firstName.equals(other.firstName))
+				return false;
+			if (lastName == null) {
+				if (other.lastName != null)
+					return false;
+			} else if (!lastName.equals(other.lastName))
+				return false;
+			return true;
+		}
+	}
+}
